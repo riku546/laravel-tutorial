8c8491bc95050bb82edf1ec0b11ff214af3251d7
riku546 <opa9788@gmail.com>
2024-08-02 07:11:52 +0900

    auth関数の導入

--- nextjs-laravel/PPP/server/app/Lib/saveProblems.php
+++ nextjs-laravel/PPP/server/app/Lib/saveProblems.php
@@ -1,16 +1,17 @@
 <?php
 //この関数は、ProblemsテーブルにAiが生成した問題文 ヒント 回答 問題のレベル プログラミング言語名を保存する関数
 
-
 namespace App\Lib;
 
 use App\Models\Problems;
+use Illuminate\Support\Facades\Auth;
 
 function saveProblems(array $res, object $request): void
 {
     try {
 
         $problem = new Problems();
+        $problem->user_id = Auth::id();
         $problem->problem = $res['problem'];
         $problem->hint = $res['hint'];
         $problem->answer = $res['answer'];
