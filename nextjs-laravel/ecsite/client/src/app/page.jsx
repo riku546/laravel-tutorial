9aba85ebcb22e8071aef4cd81b70c597cc3172b9
riku <opa9788@gmail.com>
2024-10-04 12:44:20 +0900

    topページに商品を表示するようにした

--- nextjs-laravel/ecsite/client/src/app/page.jsx
+++ nextjs-laravel/ecsite/client/src/app/page.jsx
@@ -1,10 +1,18 @@
 import Header from '@/components/selfMade/Header'
+import ProductCard from '@/components/selfMade/ProductCard'
+import styles from '@/styles/pages/topPage.module.css'
+import './global.css'
 
 const Home = () => {
+    const product_infos = []
     return (
-        <div>
+        <div className="container">
             <Header></Header>
-            <main></main>
+            <main className={styles.mainArea}>
+                <ProductCard></ProductCard>
+                <ProductCard></ProductCard>
+                <ProductCard></ProductCard>
+            </main>
         </div>
     )
 }
---
+++ nextjs-laravel/ecsite/client/src/components/selfMade/ProductCard.jsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import CardActionArea from '@mui/material/CardActionArea'
+import styles from '@/styles/components/ProductCard.module.css'
+
+export default function ProductCard() {
+    return (
+        <Card className={styles.card}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    image="/view-water-with-plastic-bottle.jpg"
+                    alt="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                    </Typography>
+                    <Typography
+                        variant="body2"
+                        sx={{ color: 'text.secondary' }}>
+                        Lizards are a widespread group of squamate reptiles,
+                        with over 6,000 species, ranging across all continents
+                        except Antarctica
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    )
+}
