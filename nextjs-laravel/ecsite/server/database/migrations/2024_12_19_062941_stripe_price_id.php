6bc2e9ab34024fc46bba8b782912f238e2e2b2b8
riku <opa9788@gmail.com>
2024-12-21 13:07:35 +0900

    複数の商品を決済するためのapiを書いた

--- nextjs-laravel/ecsite/server/database/migrations/2024_12_19_062941_stripe_price_id.php
+++ nextjs-laravel/ecsite/server/database/migrations/2024_12_19_062941_stripe_price_id.php
@@ -9,14 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+
+    //このテーブルはstripeの決済をするために必要なstripe_price_idを保存するテーブル
     public function up(): void
     {
         Schema::create('stripe_price_id', function (Blueprint $table) {
             $table->id();
+            //stripeのprice_id
             $table->string("price_id");
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
             $table->unsignedBigInteger("product_id");
             $table->foreign("product_id")->references("id")->on("products")->onDelete("cascade");
         });
+
     }
 
     /**
